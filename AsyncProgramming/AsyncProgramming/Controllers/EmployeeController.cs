67ebb6c98f8238fb56f0623a2ee74a3aa51bb811
Vidhi <vidhi.m@simformsolutions.com>
2022-12-20 12:21:25 +0530

    Async programming

---
+++ AsyncProgramming/AsyncProgramming/Controllers/EmployeeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AsyncProgramming;
+
+namespace AsyncProgramming.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeEntities db = new EmployeeEntities();
+       
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Async_Employee.ToListAsync());
+        }
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Async_Employee async_Employee = await db.Async_Employee.FindAsync(id);
+            if (async_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(async_Employee);
+        }
+
+      
+        public ActionResult Create()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,EmailId,City,IsActive")] Async_Employee async_Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Async_Employee.Add(async_Employee);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(async_Employee);
+        }
+        
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Async_Employee async_Employee = await db.Async_Employee.FindAsync(id);
+            if (async_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(async_Employee);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken ]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,EmailId,City,IsActive")] Async_Employee async_Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(async_Employee).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(async_Employee);
+        }
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Async_Employee async_Employee = await db.Async_Employee.FindAsync(id);
+            if (async_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(async_Employee);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Async_Employee async_Employee = await db.Async_Employee.FindAsync(id);
+            db.Async_Employee.Remove(async_Employee);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ReadCSV/ConsoleApp4/Program.cs
@@ -0,0 +1,69 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1();
+            Method2();
+            Method3();
+            Console.ReadKey();
+
+        }
+        public static async Task Method1()
+        {           
+            await Task.Run(()=>
+            {
+                string path = (@"C:\Users\vidhi\Desktop\Blazor\ReadCSV\CSVFiles\Employee_Vidhi_1.csv");
+                string[] lines = System.IO.File.ReadAllLines(path);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine("Employee_Vidhi_1" + " ---> "+line);
+                    Console.WriteLine();                   
+                }                   
+            });           
+        }
+
+        public static async Task Method2()
+        {            
+            await Task.Run(() =>
+            {
+                string path = (@"C:\Users\vidhi\Desktop\Blazor\ReadCSV\CSVFiles\Employee_Vidhi_2.csv");
+                string[] lines = System.IO.File.ReadAllLines(path);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine("Employee_Vidhi_2" + " ---> " + line);
+                    Console.WriteLine();
+                }
+            });
+        }
+
+        public static async Task Method3()
+        {           
+            await Task.Run(() =>
+            {
+                string path = (@"C:\Users\vidhi\Desktop\Blazor\ReadCSV\CSVFiles\Employee_Vidhi_3.csv");
+                string[] lines = System.IO.File.ReadAllLines(path);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine("Employee_Vidhi_3" + " ---> " + line);
+                    Console.WriteLine();
+                }                   
+            });
+        }
+    }
+}
+
